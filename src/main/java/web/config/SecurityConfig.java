35d38ec3028001879e1e32ee6e49203a4be0f9a6
Charik635 <cuber1319@gmail.com>
2021-07-22 01:53:08 +0300

    All Works

--- src/main/java/web/config/SecurityConfig.java
+++ src/main/java/web/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -71,6 +72,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public static NoOpPasswordEncoder passwordEncoder() {
         return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
     }
+
+
 }
 
 
--- src/main/java/web/controllers/AdminController.java
+++ src/main/java/web/controllers/AdminController.java
@@ -12,27 +12,27 @@ import web.service.UserService;
 import javax.validation.Valid;
 
 @Controller
-@RequestMapping("/")
+@RequestMapping("/admin")
 public class AdminController {
     @Autowired
     private UserService userService;
-    @GetMapping("/admin")
+    @GetMapping()
     public String index(Model model) {
         model.addAttribute("users", userService.listOfUser());
         return "index";
     }
 
-    @GetMapping("admin/{id}")
+    @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model) {
         model.addAttribute("user", userService.getUserById(id));
         return "show";
     }
-    @GetMapping("/admin/{id}/edit")
+    @GetMapping("/{id}/edit")
     public String edit(Model model, @PathVariable("id") int id) {
         model.addAttribute("user", userService.getUserById(id));
         return "edit";
     }
-    @PatchMapping("/admin/{id}")
+    @PatchMapping("/{id}")
     public String update(@ModelAttribute("user") @Valid User user, BindingResult bindingResult, @PathVariable("id") int id) {
         if (bindingResult.hasErrors()) {
             return "edit";
@@ -40,18 +40,18 @@ public class AdminController {
         userService.updateUser(id, user);
         return "redirect:/admin";
     }
-    @DeleteMapping("/admin/{id}")
+    @DeleteMapping("/{id}")
     public String delete(@PathVariable("id") int id) {
         userService.removeUser(id);
         return "redirect:/admin";
     }
-    @GetMapping("admin/newuser")
+    @GetMapping("/newuser")
     public String newUser(Model model) {
         model.addAttribute("user", new User());
         return "new";
     }
 
-    @PostMapping("/admin/create")
+    @PostMapping("/create")
     public String create(@ModelAttribute("user") @Valid User user, BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "new";
